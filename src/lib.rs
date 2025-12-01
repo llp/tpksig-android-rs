1313f43a5d2c4c6ace78c887517e69ed7c71d63c
liulipeng <pengliliu8086@163.com>
2025-12-01 16:20:08 +0800

    init project

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
 use jni::objects::{JClass, JString};
-use jni::sys::{jboolean, jint};
-use jni::sys::{JNI_FALSE, JNI_TRUE};
+use jni::sys::jint;
 use jni::JNIEnv;
 use std::path::PathBuf;
 
 #[no_mangle]
 pub extern "system" fn JNI_OnLoad(_vm: jni::JavaVM, _reserved: *mut std::ffi::c_void) -> jint {
-    android_logger::init_once(
-        android_logger::Config::default()
-            .with_max_level(log::LevelFilter::Error)
-            .with_tag("TpkSig"),
-    );
     jni::sys::JNI_VERSION_1_6
 }
 
@@ -19,13 +13,12 @@ pub extern "system" fn Java_com_quicktvui_TpkSig_verify(
     mut env: JNIEnv,
     _class: JClass,
     path: JString,
-) -> jboolean {
+) -> jint {
     // 1. JString -> Rust String
     let path_rust: String = match env.get_string(&path) {
         Ok(jstr) => jstr.into(),
         Err(_) => {
-            log::error!("Failed to convert JString to Rust String");
-            return JNI_FALSE;
+            return 1;
         }
     };
 
@@ -36,16 +29,14 @@ pub extern "system" fn Java_com_quicktvui_TpkSig_verify(
     let tpk = match tpksig::Tpk::new(path_buf) {
         Ok(tpk) => tpk,
         Err(e) => {
-            log::error!("Tpk::new failed: {}", e);
-            return JNI_FALSE;
+            return 1;
         }
     };
 
     match tpk.verify() {
-        Ok(_) => JNI_TRUE,
+        Ok(_) => 0,
         Err(e) => {
-            log::error!("tpk.verify() failed: {}", e);
-            JNI_FALSE
+            return 1;
         }
     }
 }
